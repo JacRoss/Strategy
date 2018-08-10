b4fd548cebc392e1eefa64b919806c928977a92c
JacRoss <tori.belechy@gmail.com>
2018-08-10 18:57:46 +0300

    -

--- src/Operations/Division.php
+++ src/Operations/Division.php
@@ -5,6 +5,13 @@ namespace Jackross\Strategy\Operations;
 class Division implements IOperation
 {
 
+    /**
+     * @param int $a
+     * @param int $b
+     * @return int
+     *
+     * @throws \DivisionByZeroError
+     */
     public function calculate(int $a, int $b): int
     {
         return intdiv($a, $b);
